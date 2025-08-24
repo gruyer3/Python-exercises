12420099d08d7ef361f918b0296b4da097c1a040
gruyer3 <gr00.y3re@gmail.com>
2025-08-24 21:55:29 +0200

    some more explanations regarding numbers

--- 0-fundamentals/3-numbers.py
+++ 0-fundamentals/3-numbers.py
@@ -2,21 +2,27 @@
 # int = whole numbers
 # float = numbers with decimals
 int = 1024
+print(type(int))
 float = 2.137
+print(type(float))
 
 # Arithmetic operations
 # +, -, *, /
+print ("2 + 2 =", 2 + 2)
+print ("4 - 1 =", 4 - 1)
+print ("3 * 8 =", 3 * 8)
 # / -> true division (float)
+print ("8 / 4 =", 8 / 4)
 # // -> floor division (int or float)
+print ("8 // 4 =", 8 // 4)
 # % -> modulo (reminder)
+print ("48 % 13 =", 48 % 13)
 # ** -> power
-"""
->>> 2 / 2
-1.0
->>> 2 // 2
-1
->>> 48 % 13
-9
->>> 2 ** 3
-8
-"""
+print ("3 ** 2 =", 3 ** 2)
+
+
+# When comparing floats directly, we may run into precision issues:
+print ("0.1 * 3 == 0.3:", 0.1 * 3 == 0.3)
+# To tackle this, we can use the math.isclose() function:
+import math
+print ("math.isclose(0.1 * 3, 0.3):", math.isclose(0.1 * 3, 0.3))
