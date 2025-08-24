e82a5b37d77e89cb0d79f1f4c060a44c56a01a11
gruyer3 <gr00.y3re@gmail.com>
2025-08-24 21:41:59 +0200

    numbers and comments explained

---
+++ 0-fundamentals/2-comments.py
@@ -0,0 +1,10 @@
+# Single line comments can be created by using hashtag (#) symbol. 
+# They're best for explaining why, adding TODO/FIXME markers or temporarily disabling code.
+# Example of single line comment:
+error_code = 0 # Initializing error counter
+# TODO: handle case when argument is None
+# Focus on commenting more complex lines
+
+"""
+It is possible to wrap multiline comments between triple single-quotes or between triple double quotes.
+"""
---
+++ 0-fundamentals/3-numbers.py
@@ -0,0 +1,22 @@
+# Numbers - int & float
+# int = whole numbers
+# float = numbers with decimals
+int = 1024
+float = 2.137
+
+# Arithmetic operations
+# +, -, *, /
+# / -> true division (float)
+# // -> floor division (int or float)
+# % -> modulo (reminder)
+# ** -> power
+"""
+>>> 2 / 2
+1.0
+>>> 2 // 2
+1
+>>> 48 % 13
+9
+>>> 2 ** 3
+8
+"""
